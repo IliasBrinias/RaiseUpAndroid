3b0992191a4feaabd76eacf8cb0e921600f245c0
Ilias Brinias <elliotpatra@gmail.com>
2023-11-11 20:52:02 +0200

    RegisterLayout, LoginLayout and MainLayout completed

--- app/src/main/java/com/unipi/msc/raiseupandroid/Activity/MainActivity.java
+++ app/src/main/java/com/unipi/msc/raiseupandroid/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.unipi.msc.raiseupandroid;
+package com.unipi.msc.raiseupandroid.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.unipi.msc.raiseupandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
